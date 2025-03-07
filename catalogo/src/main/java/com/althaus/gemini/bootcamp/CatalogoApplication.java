6c91bd01f8851ec463a5b2d8020f7d9c067f0706
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-07 09:12:59 +0100

    added comandLineRunner added port configuration to application.properties

--- catalogo/src/main/java/com/althaus/gemini/bootcamp/CatalogoApplication.java
+++ catalogo/src/main/java/com/althaus/gemini/bootcamp/CatalogoApplication.java
@@ -1,13 +1,27 @@
 package com.althaus.gemini.bootcamp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CatalogoApplication {
+public class CatalogoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub		
+	}
+	
+	@Bean
+	CommandLineRunner demo() {
+		return(args)->{
+			System.err.println("Aplicacion arrancada");
+		};
 	}
 
 }
