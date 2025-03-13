dbc947635d8e1a1fd6a02f1ea970bd60395b3c21
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-13 12:04:46 +0100

    Updated application.properties and added autowire repositories to application main class

--- catalogo/src/main/java/com/althaus/gemini/bootcamp/CatalogoApplication.java
+++ catalogo/src/main/java/com/althaus/gemini/bootcamp/CatalogoApplication.java
@@ -1,12 +1,30 @@
 package com.althaus.gemini.bootcamp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.althaus.gemini.bootcamp.domains.contracts.repositories.ActorRepository;
+import com.althaus.gemini.bootcamp.domains.contracts.repositories.CategoryRepository;
+import com.althaus.gemini.bootcamp.domains.contracts.repositories.FilmRepository;
+import com.althaus.gemini.bootcamp.domains.contracts.repositories.LanguageRepository;
+
 @SpringBootApplication
 public class CatalogoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ActorRepository actorRepository; 
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private FilmRepository filmRepository;
+	
+	@Autowired
+	private LanguageRepository languajeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
@@ -14,7 +32,7 @@ public class CatalogoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub		
+		actorRepository.findTop10ByFirstNameStartingWithOrderByLastNameDesc("r").forEach(System.err::println);;	
 	}
 	
 	@Bean
--- catalogo/src/main/java/com/althaus/gemini/bootcamp/domains/contracts/repositories/ActorRepository.java
+++ catalogo/src/main/java/com/althaus/gemini/bootcamp/domains/contracts/repositories/ActorRepository.java
@@ -3,7 +3,7 @@ package com.althaus.gemini.bootcamp.domains.contracts.repositories;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
