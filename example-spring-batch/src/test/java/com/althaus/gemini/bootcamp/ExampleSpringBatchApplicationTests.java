fe3a9f93f279e16eb1980fb282aede9183e85ef0
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-18 12:04:46 +0100

    initial commit

---
+++ example-spring-batch/src/test/java/com/althaus/gemini/bootcamp/ExampleSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.althaus.gemini.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
