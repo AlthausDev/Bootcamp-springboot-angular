29293180f112273f44d8fbc99a9cac399bae3228
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-24 15:16:42 +0100

    Initial commit

---
+++ gs-soap-service/src/main/java/com/althaus/gemini/bootcamp/GsSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.althaus.gemini.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSoapServiceApplication.class, args);
+	}
+
+}
