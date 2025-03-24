caa1a93fdb9cd05a5c6fd0991bb3f2a0baa842df
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-24 15:57:53 +0100

    added consuming web service

---
+++ gs-consuming-web-service/src/main/java/com/althaus/gemini/bootcamp/GsConsumingWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.althaus.gemini.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsConsumingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsConsumingWebServiceApplication.class, args);
+	}
+
+}
