a1b71f69fccbbb63e82795237995fe308e4250f0
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-25 15:12:12 +0100

    chore

--- catalogo/src/main/java/com/althaus/gemini/bootcamp/CatalogoApplication.java
+++ catalogo/src/main/java/com/althaus/gemini/bootcamp/CatalogoApplication.java
@@ -1,8 +1,17 @@
 package com.althaus.gemini.bootcamp;
 
+import java.util.TreeMap;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CatalogoApplication implements CommandLineRunner {
@@ -14,5 +23,37 @@ public class CatalogoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicacion arrancada");
+	}
+
+	@Bean
+	public OpenApiCustomizer sortSchemasAlphabetically() {
+	    return openApi -> {
+	        var schemas = openApi.getComponents().getSchemas();
+	        openApi.getComponents().setSchemas(new TreeMap<>(schemas));
+	    };
+	}
+	
+	@Bean
+	@Primary
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplateLB(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@LoadBalanced
+	@Bean
+	RestClient.Builder restClientBuilderLB() {
+		return RestClient.builder();
+	}
+
+	@Primary
+	@Bean
+	RestClient.Builder restClientBuilder() {
+		return RestClient.builder();
 	}
 }
