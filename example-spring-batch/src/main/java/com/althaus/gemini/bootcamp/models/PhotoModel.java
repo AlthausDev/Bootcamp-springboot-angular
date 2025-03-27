f1cbc8b159732c6b7064645d2e07e5c08a40fb8b
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-27 11:06:14 +0100

    chore

--- example-spring-batch/src/main/java/com/althaus/gemini/bootcamp/models/PhotoModel.java
+++
@@ -1,9 +0,0 @@
-package com.althaus.gemini.bootcamp.models;
-
-import lombok.Data;
-
-@Data
-public class PhotoModel {
-	private String id, author, url, download_url;
-	private int width, height;
-}
