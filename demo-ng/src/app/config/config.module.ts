763bd92ffe545781f69b156eb6cc492df71f66bf
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-28 09:52:49 +0100

    added config and security modules

--- demo-ng/src/app/config/config.module.ts
+++ demo-ng/src/app/config/config.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyCoreModule } from '@my/core';
 
 
 
@@ -7,7 +8,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   exports: [],
   imports: [
-    CommonModule
+    CommonModule, MyCoreModule
   ]
 })
 export class ConfigModule { }
--- demo-ng/src/app/security/security.module.ts
+++ demo-ng/src/app/security/security.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MyCoreModule } from '@my/core';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, MyCoreModule
   ]
 })
 export class SecurityModule { }
