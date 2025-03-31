ddcc47e65947391b8b4a05b93280441be844cdd4
salthaus <samuel.althaus-alonso@capgemini.com>
2025-03-31 11:33:28 +0200

    added home component

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -3,20 +3,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
 import { NotificationComponent } from './main/notification/notification.component';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, NotificationComponent],
+  imports: [RouterOutlet, CommonModule, NotificationComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'demo-ng';
 
-  constructor(logger: LoggerService) {
-    logger.log('AppComponent created LOG');
-    logger.info('AppComponent created INFO');
-    logger.warn('AppComponent created WARN');
-    logger.error('AppComponent created ERROR');
-  }
+  // constructor(logger: LoggerService) {
+  //   logger.log('AppComponent created LOG');
+  //   logger.info('AppComponent created INFO');
+  //   logger.warn('AppComponent created WARN');
+  //   logger.error('AppComponent created ERROR');
+  // }
 }
